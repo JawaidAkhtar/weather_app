c5ab01ff2357e4d9e60c04ae39443aa2b42b6fdb
Jawaid Akhtar <139637049+JawaidAkhtar@users.noreply.github.com>
2024-08-19 19:50:07 +0530

    added day,date,time and next day forecast

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import requests
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
@@ -15,19 +16,47 @@ def weather():
     return render_template('index.html', weather=weather_data)
 
 def get_weather_data(city, api_key):
+    # Current weather data
     url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
     response = requests.get(url)
     data = response.json()
+
+    # Next day forecast data
+    forecast_url = f'http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={api_key}&units=metric'
+    forecast_response = requests.get(forecast_url)
+    forecast_data = forecast_response.json()
+
+    # Get current date and time
+    current_time = datetime.now()
+    next_day = current_time + timedelta(days=1)
+    next_day_weather = None
+
+    # Find the weather forecast for the next day (24 hours later)
+    for forecast in forecast_data['list']:
+        forecast_time = datetime.fromtimestamp(forecast['dt'])
+        if forecast_time.date() == next_day.date():
+            next_day_weather = {
+                'temperature': forecast['main']['temp'],
+                'description': forecast['weather'][0]['description'],
+                'icon': forecast['weather'][0]['icon']
+            }
+            break
+
     if data['cod'] == 200:
         return {
             'city': data['name'],
             'temperature': data['main']['temp'],
             'description': data['weather'][0]['description'],
-            'icon': data['weather'][0]['icon']
+            'icon': data['weather'][0]['icon'],
+            'day': current_time.strftime('%A'),
+            'date': current_time.strftime('%Y-%m-%d'),
+            'time': current_time.strftime('%H:%M:%S'),
+            'next_day_weather': next_day_weather
         }
     else:
         return None
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
+
 
